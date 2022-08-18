fd2a7f453e97313526d8bf3e68a113172cd17b36
Asis <asiso7@gmail.com>
2022-08-18 11:14:19 +0200

    Dash App with Pie Chart

--- my_app/app.py
+++ my_app/app.py
@@ -1,8 +1,13 @@
-import numpy as np
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
-from shiny import App, reactive, render, ui
 from functions import *
+
+
+app = Dash(__name__)
 
 
 # get investor df
@@ -10,51 +15,45 @@ investor_df = get_investor_data()
 
 # generate list of investors
 investors_array = investor_df.sort_values('investor')["investor"].values
-investors_list = investors_array.tolist()
-#investor_dict = dict(enumerate(investors_array.flatten(), 1))
-#investor_dict = {'a': 1, 'b': 2}
+#investors_list = investors_array.tolist()
 
-app_ui = ui.page_fluid(
-    ui.h2("Superinvestors Portfolio"),
-    ui.input_select("name", "Investor", investors_list),
-    ui.output_plot("plot"),
-    ui.output_text("name")
-)
-
-
-def server(input, output, session):
-
-    # initiliaze 
-    
-    
-    # Pie Chart
-    """ fig = px.pie(values=portfolio_df['% ofPortfolio'], names=portfolio_df['Stock'])
-    fig = fig.show()
-    register_widget("fig", fig)
-
-    @reactive.Effect
-    def _():
-        fig.name = input.name() """
-    @output
-    @render.plot    
-    def plot():
-        search_result_url, search_result_investor = search_investor(input.name(), investor_df)
-        portfolio_df = get_portfolio_data(search_result_url, search_result_investor)
-        y = portfolio_df['% ofPortfolio']
-        labels = portfolio_df['Stock']
-        fig = plt.pie(y, labels = labels, startangle = 90)
-
-        return fig
-    
-    @output
-    @render.text
-    def name():
-        return f"The current investor is {input.name()}" 
-      
         
+    
+app.layout = html.Div(children=[
+    
+    html.H1(children="Superinvestor's Portfolios"),
+
+    html.Div(children='''
+        A simple Dash Application on top of the DataRoma.com
+    '''),
+    
+    html.Br(),
+    html.Br(),
+    html.Label('Select investor'),
+        dcc.Dropdown(
+            investors_array, 
+            value=investors_array[0],
+            id='investor-select'),
+        
+    dcc.Graph(id='pie-chart')
+])
+
+@app.callback(
+    Output('pie-chart', 'figure'),
+    Input('investor-select', 'value'))
+def update_figure(investor_name):
+    search_result_url, search_result_investor = search_investor(investor_name, investor_df)
+    portfolio_df = get_portfolio_data(search_result_url, search_result_investor)
+
+    y = portfolio_df['% ofPortfolio']
+    labels = portfolio_df['Stock']
+    fig = px.pie(values=y, names=labels)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
 
-app = App(app_ui, server)
-
- 
- 
